cf5e2b18c679bf441c8409e8a0821098535f7d32
Svmpankaj <svmpankaj@gmail.com>
2023-03-14 19:53:09 +0530

    using fetch api to populate newsitems

--- src/components/News.js
+++ src/components/News.js
@@ -2,11 +2,37 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+
+    constructor() {
+        super();
+        console.log("Hello I am a constructor from News component");
+        this.state = {
+            articles: [],
+            loading: false
+        }
+    }
+
+    async componentDidMount() {
+        let url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=a6af2a49112b4b3fbc537d4335a74426"
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({ articles: parsedData.articles })
+    }
+
     render() {
         return (
-            <div>
-                This is news component
-                <NewsItem />
+            <div className='container my-3'>
+                <h1>NewsMonkey - Top Headlines</h1>
+
+                <div className="row">
+                    {this.state.articles.map((element) => {
+                        return <div className="col-md-4" key={element.url}>
+                            <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
+                        </div>
+                    })}
+
+                </div>
             </div>
         )
     }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+
+
     render() {
+        let { title, description, imageUrl, newsUrl } = this.props
         return (
-            <div>
-                This is a news item
+            <div className='my-3'>
+                <div className="card" style={{ width: "18rem" }}>
+                    <img src={!imageUrl ? "https://techcrunch.com/wp-content/uploads/2023/03/TIExplorer_WorldRadioDay_PR_1080x1080.jpg?w=1074" : imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+                    </div>
+                </div>
             </div>
         )
     }
