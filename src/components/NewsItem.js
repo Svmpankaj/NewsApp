1e6443c4b2e78e605232f9a8c5f498d185daa5d6
Svmpankaj <svmpankaj@gmail.com>
2023-03-19 13:46:24 +0530

    adding a top loading bar

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,8 +8,10 @@ export class NewsItem extends Component {
         return (
             <div className='my-3'>
                 <div className="card" >
-                    <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{ left: '90%', zIndex: '1' }}> {source}
-                    </span>
+                    <div style={{ display: 'flex', justifyContent: 'flex-end', position: 'absolute', right: '0' }}>
+                        <span className=" badge rounded-pill bg-danger" style={{ left: '90%', zIndex: '1' }}> {source}
+                        </span>
+                    </div>
                     <img src={!imageUrl ? "https://techcrunch.com/wp-content/uploads/2023/03/TIExplorer_WorldRadioDay_PR_1080x1080.jpg?w=1074" : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title} </h5>
