45ac07dd8d7ce16e769cfc944162eff6ae227fbf
Svmpankaj <svmpankaj@gmail.com>
2023-03-20 20:34:50 +0530

    app.js change in functional component

--- src/components/News.js
+++ src/components/News.js
@@ -10,7 +10,6 @@ const News = (props) => {
     const [loading, setLoading] = useState(true)
     const [page, setPage] = useState(1)
     const [totalResults, setTotalResults] = useState(0)
-    // document.title = `${capitalizeFirstLetter(props.category)} - NewsMonkey`;
 
     const capitalizeFirstLetter = (string) => {
         return string.charAt(0).toUpperCase() + string.slice(1);
@@ -32,23 +31,15 @@ const News = (props) => {
     }
 
     useEffect(() => {
+        document.title = `${capitalizeFirstLetter(props.category)} - NewsMonkey`;
         updateNews();
-
+        // eslint-disable-next-line
     }, [])
 
-    const handlePrevClick = async () => {
-        setPage(page - 1)
-        updateNews();
-    }
-
-    const handleNextClick = async () => {
-        setPage(page + 1)
-        updateNews();
-    }
 
     const fetchMoreData = async () => {
-        setPage(page + 1)
-        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apikey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`
+        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apikey=${props.apiKey}&page=${page + 1}&pageSize=${props.pageSize}`;
+        setPage(page + 1);
         this.setState({ loading: true })
         let data = await fetch(url);
         let parsedData = await data.json();
@@ -61,8 +52,8 @@ const News = (props) => {
 
     return (
         <>
-            <h1 className="text-center" style={{ margin: '35px 0px' }}>NewsMonkey - Top {capitalizeFirstLetter(props.category)} Headlines </h1>
-            {/* {this.state.loading && <Spinner />} */}
+            <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '90px' }}>NewsMonkey - Top {capitalizeFirstLetter(props.category)} Headlines </h1>
+            {loading && <Spinner />}
             <InfiniteScroll
                 dataLength={articles.length}
                 next={fetchMoreData}
